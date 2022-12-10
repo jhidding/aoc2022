fa65a3b08575000f7ddb8c9ca6733a34e2a55cba
Johan Hidding <j.hidding@esciencecenter.nl>
2022-12-10 20:48:00 +0100

    ready for day11

---
+++ docs/src/day11.md
@@ -0,0 +1,13 @@
+# Day 11:
+
+``` {.julia file=src/day11.jl}
+module Day11
+
+function read_input(inp::IO)
+end
+
+function main(inp::IO, out::IO)
+end
+
+end  # module
+```
---
+++ src/day11.jl
@@ -0,0 +1,12 @@
+# ~\~ language=Julia filename=src/day11.jl
+# ~\~ begin <<docs/src/day11.md|src/day11.jl>>[init]
+module Day11
+
+function read_input(inp::IO)
+end
+
+function main(inp::IO, out::IO)
+end
+
+end  # module
+# ~\~ end
